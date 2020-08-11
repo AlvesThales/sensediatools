79614b3e43c951ba3d9077f2fef1099bbdf51e33
AlvesThales <thalesguimaraes@gmail.com>
2020-08-11 17:39:43 -0300

    Updates

---
+++ precache-manifest.a167da21dea7c7eed879fe450adc6513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987c6c8d849d8951e72189ee4394e5d2",
+    "url": "/sensediatools/index.html"
+  },
+  {
+    "revision": "0778c26320689c1a8b4a",
+    "url": "/sensediatools/static/css/2.8b81dbfa.chunk.css"
+  },
+  {
+    "revision": "6a0cc4fdf4aa699b50c4",
+    "url": "/sensediatools/static/css/main.f1363884.chunk.css"
+  },
+  {
+    "revision": "0778c26320689c1a8b4a",
+    "url": "/sensediatools/static/js/2.dcb00858.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/sensediatools/static/js/2.dcb00858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a0cc4fdf4aa699b50c4",
+    "url": "/sensediatools/static/js/main.c3604b04.chunk.js"
+  },
+  {
+    "revision": "8dc4931a74304320415f",
+    "url": "/sensediatools/static/js/runtime-main.939b7d7b.js"
+  }
+]);
